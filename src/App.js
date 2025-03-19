7d4e2f46339063d97fc4f5607c680ace843a5790
Dineshok <dhineshok@gmail.com>
2025-03-19 17:29:27 +0530

    added basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/FoodDelivery">
         <Navbar />
         <Home />
         <Routes>
